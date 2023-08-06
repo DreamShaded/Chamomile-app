8012dbcf07dc0e7d18ea105dfdc6cae818acc546
Роман Юрьевич Троицкий <vzhyx@ya.ru>
2023-08-06 21:44:44 +0300

    feat: v1

---
+++ v2/README.md
@@ -0,0 +1,4 @@
+# Пример приложния с Chamomile UI
+
+Chamomile UI - UI-кит для VueJS v3.
+Специально для [MSK VueJS](https://t.me/msk_vue_js)
---
+++ v2/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="page">
+    <ChamomileInput
+    />
+    <ChamomileDatepicker
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import ChamomileInput from "Chamomile-UI/src/components/ChamomileInput.vue";
+import ChamomileDatepicker from "Chamomile-UI/src/components/ChamomileDatepicker.vue";
+
+</script>
